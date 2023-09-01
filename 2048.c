dac47939da8b4d914a7eb9f7b8d4e6b5a63a52b0
JJRubes <jamesfittongum@gmail.com>
2023-09-01 13:56:22 +0930

    v1

--- 2048.c
+++ 2048.c
@@ -3,6 +3,8 @@
 #error "custom error"
 #endif
 
+#define _ -1
+
 void p(int board[4][4]) {
   printf("%4d %4d %4d %4d\n", board[0][0], board[0][1], board[0][2], board[0][3]);
   printf("%4d %4d %4d %4d\n", board[1][0], board[1][1], board[1][2], board[1][3]);
@@ -10,24 +12,71 @@ void p(int board[4][4]) {
   printf("%4d %4d %4d %4d\n\n", board[3][0], board[3][1], board[3][2], board[3][3]);
 }
 
+int move(int board[16], int direction, int pos) {
+  /* part 1: find the next number 
+     _ _ 2 2
+     ^ no number
+     _ _ 2 2
+       ^ no number
+     _ _ 2 2
+         ^ number
+     2 _ _ 2
+
+     part 2: find the combination
+     2 _ _ 2
+       ^ not 2
+     2 _ _ 2
+         ^ not 2
+     2 _ _ 2
+           ^ 2
+     4 _ _ _
+
+     this might be doable as one function
+     */
+
+  if(board[pos] == _) {
+    // extra cycle?
+    // bounds check is wrong
+    // divide by direction, then abs, then 0 <= . < 4
+    for(int i = pos; i < 16 && i > _; i += direction) {
+      if(board[i] != _) {
+        board[pos] = board[i];
+        board[i] = _;
+        break;
+      }
+    }
+  }
+
+  if(board[pos] != _) {
+    for(int i = pos + direction; i < 16 && i > _; i += direction) {
+      if(board[i] = board[pos]) {
+        board[pos] *= 2;
+        board[i] = _;
+        break;
+      }
+    }
+  }
+
+  return board[pos];
+}
+
 int main() {
-  int _ = -1;
-  int board[4][4] = {
+  int board[16] = {
  /* +------+------+------+------+
     |      |      |      |      | */
-    {    2 ,    2 ,    2 ,    2 },
+         2 ,    2 ,    2 ,    2 ,
  /* |      |      |      |      |
     +------+------+------+------+
     |      |      |      |      | */
-    {    _ ,    _ ,    _ ,    _ },
+         _ ,    _ ,    _ ,    _ ,
  /* |      |      |      |      |
     +------+------+------+------+
     |      |      |      |      | */
-    {    _ ,    _ ,    _ ,    _ },
+         _ ,    _ ,    _ ,    _ ,
  /* |      |      |      |      |
     +------+------+------+------+
     |      |      |      |      | */
-    {    _ ,    _ ,    _ ,    _ },
+         _ ,    _ ,    _ ,    _ ,
  /* |      |      |      |      |
     +------+------+------+------+ */
   };
